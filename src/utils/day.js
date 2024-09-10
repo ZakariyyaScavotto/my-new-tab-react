eb12d6b33fe819d5575ed55ae1d73eaaca723fc2
ZakariyyaScavotto <zakscav4@gmail.com>
2024-09-09 21:23:26 -0400

    Forgot to reset the day updating logic from summer

--- src/utils/day.js
+++ src/utils/day.js
@@ -11,6 +11,7 @@ const hide = (classname) => {
 	}
 };
 export const setDisplay = (day) => {
+	/*
 	show("Summer");
 	hide("monday");
 	hide("tuesday");
@@ -18,49 +19,48 @@ export const setDisplay = (day) => {
 	hide("thursday");
 	hide("friday");
 	hide("weekend");
-	/*
-	if (day["currentDay"] === 1) {
-		show("monday")
-		hide("tuesday")
-		hide("wednesday")
-		hide("thursday")
-		hide("friday")
-		hide("weekend")
-	} else if (day["currentDay"] === 2) {
-		hide("monday")
-		show("tuesday")
-		hide("wednesday")
-		hide("thursday")
-		hide("friday")
-		hide("weekend")
-	} else if (day["currentDay"] === 3) {
-		hide("monday")
-		hide("tuesday")
-		show("wednesday")
-		hide("thursday")
-		hide("friday")
-		hide("weekend")
-	} else if (day["currentDay"] === 4) {
-		hide("monday")
-		hide("tuesday")
-		hide("wednesday")
-		show("thursday")
-		hide("friday")
-		hide("weekend")
-	} else if (day["currentDay"] === 5) {
-		hide("monday")
-		hide("tuesday")
-		hide("wednesday")
-		hide("thursday")
-		show("friday")
-		hide("weekend")
-	} else {
-		hide("monday")
-		hide("tuesday")
-		hide("wednesday")
-		hide("thursday")
-		hide("friday")
-		show("weekend")
-	}
 	*/
+	if (day["currentDay"] === 1) {
+		show("monday");
+		hide("tuesday");
+		hide("wednesday");
+		hide("thursday");
+		hide("friday");
+		hide("weekend");
+	} else if (day["currentDay"] === 2) {
+		hide("monday");
+		show("tuesday");
+		hide("wednesday");
+		hide("thursday");
+		hide("friday");
+		hide("weekend");
+	} else if (day["currentDay"] === 3) {
+		hide("monday");
+		hide("tuesday");
+		show("wednesday");
+		hide("thursday");
+		hide("friday");
+		hide("weekend");
+	} else if (day["currentDay"] === 4) {
+		hide("monday");
+		hide("tuesday");
+		hide("wednesday");
+		show("thursday");
+		hide("friday");
+		hide("weekend");
+	} else if (day["currentDay"] === 5) {
+		hide("monday");
+		hide("tuesday");
+		hide("wednesday");
+		hide("thursday");
+		show("friday");
+		hide("weekend");
+	} else {
+		hide("monday");
+		hide("tuesday");
+		hide("wednesday");
+		hide("thursday");
+		hide("friday");
+		show("weekend");
+	}
 };
